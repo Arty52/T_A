f48c7e5cd5c1b0481a2568187dfe5b7284f27b36
Arty52 <artgrichine@gmail.com>
2015-06-14 21:22:38 -0500

    project started, background set

---
+++ Assets/ChangeOnClick.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ChangeOnClick : MonoBehaviour {
+
+	public Transform quadLocation;
+	public GameObject stripeImage;
+
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetButton ("Fire1")) 
+		{	// Change card
+			Instantiate (stripeImage, quadLocation.position, quadLocation.rotation);
+		}
+	}
+}
